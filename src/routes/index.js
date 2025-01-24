deecdb898ea4207ffe97400f2011eeada17e624e
SondreB <sondre@outlook.com>
2025-01-24 18:29:23 +0100

    Update version and return empty in API

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,15 +11,18 @@ app.use(throttleMiddleware);
 
 // Define your routes here
 app.get('/restaurants', (req, res) => {
+  res.json([]);
   // Your code to get the list of restaurants
 });
 
 app.get('/menu/:restaurantId', (req, res) => {
   // Your code to get the menu of a restaurant
+  res.json([]);
 });
 
 app.get('/menu/item/:itemId', (req, res) => {
   // Your code to get a menu item
+  res.json([]);
 });
 
 // Start the server
