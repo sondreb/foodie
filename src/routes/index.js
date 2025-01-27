5db842a2c48f11f4d7da24fcb7c1d870a0215f8a
SondreB <sondre@outlook.com>
2025-01-27 11:13:20 +0100

    Add app domain in origin

--- src/routes/index.js
+++ src/routes/index.js
@@ -56,14 +56,15 @@ const app = express();
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
 
+// app.use(cors({origin: ['http://localhost:3000', 'http://localhost:4200', 'https://foodie.brainbox.no']}));
+
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader("Access-Control-Allow-Origin", "https://foodie.brainbox.no");
   //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   //   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.setHeader("Access-Control-Allow-Credentials", true);
-
   next();
 });
 
